2dc486ad7fe8cce8be832326b585ad40c0e729c5
zamansky <zamansky@gmail.com>
2014-03-03 14:33:58 -0500

    Z

---
+++ misc/cipher/cipher.py
@@ -0,0 +1,37 @@
+
+s="this is a sample sentence for use in testing the ceasar cipher thing"
+
+def encode1(s,n):
+    """
+    Rotate the string by n characters, leaving spaces
+    Done with blow by blow comments
+    """
+    r = ""
+    for l in s:
+        l = ord(l) # convert to ascii
+        l = l - 97 # 'a' is 97 so we want to reduce so 'a'=0 'b'=1 etc
+        l = l + n # add the offset
+        l=l%26 # use mod so that we wrap around back to 'a' if we go past 'z'
+        l=l+97 # and add back the 97
+        r = r + chr(l)
+    return r
+
+def encode2(s,n):
+    """
+    Rotate the string by n characters, leaving spaces
+    Done with blow by blow comments
+    """
+    r = [ chr(((ord(x)-97+n)%26)+97) if x!=' ' else x for x in s]
+    return "".join(r)
+
+
+import math
+def dist(a,b):
+    sum=0
+    for i in range(len(a)):
+        sum = sum + pow(a[i]-b[i],2)
+    return math.sqrt(sum)
+
+
+
+    
