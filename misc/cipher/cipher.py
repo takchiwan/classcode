9753976f808d0f7207c20a6fe430bb96f1ba5154
Mike Zamansky <zamansky@gmail.com>
2014-03-03 19:30:16 -0500

    Z

--- misc/cipher/cipher.py
+++ misc/cipher/cipher.py
@@ -1,5 +1,4 @@
 
-s="this is a sample sentence for use in testing the ceasar cipher thing"
 
 def encode1(s,n):
     """
@@ -32,6 +31,35 @@ def dist(a,b):
         sum = sum + pow(a[i]-b[i],2)
     return math.sqrt(sum)
 
+def calcPercents(s):
+    """
+    Returns a list of 26 elements L[0] will be the frequency of 
+    the letter a i the list, L[1] the letter b etc. 
 
+    You can calculate the frequence by calculating 
+    (# times the letter appears)/(total # of letters)
+    """
+    pass
 
-    
+englishPercents=[8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,
+                 6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,
+                 5.987,6.327,9.056,2.758,0.978,2.360,0.150,1.974,0.074];
+  
+
+import random
+s="this is a sample sentence for use in testing the ceasar cipher thing"
+# This is encoded message
+encmessage = encode2(s,random.randrange(26))
+
+# Your tasks
+#1. Rewrite dist so that it uses a list comprehention
+#2. Finish writing calcPercents
+#
+#3. For each of the 26 possible rotations of encmessage (the encoded message) 
+#   see the distance between it and the englishPerents. The closest should
+#   be the amount needed to decode the message
+#n
+#4. Instead of using englishPercents, download a book from project Gutenberg
+#   I'd say the Complete works of Shakespeare. Read it in and use it to 
+#   calculate letter frequencies.
+
